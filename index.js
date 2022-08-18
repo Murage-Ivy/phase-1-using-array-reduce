c43833575eada537920d2e45d87dbe17046e30cd
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-08-19 00:46:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = batteryBatches.reduce((accumulator, batteryBatch) => batteryBatch + accumulator);
+console.log(totalBatteries);
