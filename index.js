6813a356e577995a59c3dbd08a0539c4a0bbba0f
Murage-Ivy <ivymurage2000@gmail.com>
2024-03-24 00:16:04 +0300

    Add a custom reducer function

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-let totalBatteries = batteryBatches.reduce((accumulator, batteryBatch) => batteryBatch + accumulator);
+let totalBatteries = batteryBatches.reduce(
+  (accumulator, batteryBatch) => batteryBatch + accumulator
+);
+
+function myReducer(arr, reducer, initalValue) {
+  let accum = initalValue;
+  for (const element of arr) {
+    accum = reducer(accum, element);
+  }
+  return accum;
+}
+
+function batteryCounter(totalAmount, battery) {
+  return totalAmount + battery;
+}
+
+let totalBatteriess = myReducer(batteryBatches, batteryCounter, 0);
+console.log(totalBatteriess);
+console.log(totalBatteries);
